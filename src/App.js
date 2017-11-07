89c04426a6266d5ff92d440f7d6bbaeb2bcc0a9b
Christopher Fujino <christopherfujino@gmail.com>
2017-11-06 23:18:44 -0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Distant Outpost</h1>
+        <div>
+          <button>New Game</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
